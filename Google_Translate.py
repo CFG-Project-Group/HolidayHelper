9ffb276ccb405ceae5053ba297d17552f2165650
Samin Nazarpouri <nzsamin@yahoo.co.uk>
2022-11-04 16:35:33 +0000

    Added modified Gtranslate

--- Google_Translate.py
+++ Google_Translate.py
@@ -4,31 +4,33 @@ import requests
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'GoogleKey.json'
 
+text = input("input: ")
 
 class Translator:
 
-    def __init__(self):
+    def __init__(self, text):
         self.translate_client = translate_v2.Client()
+        self.text = text
 
-    def czech_translator(self, text):
+    def czech_translator(self):
         target = "cs"
-        output = self.translate_client.translate(text, target_language=target)
+        output = self.translate_client.translate(self.text, target_language=target)
         return output['translatedText'].lower()
 
-    def spanish_translator(self, text):
+    def spanish_translator(self):
         target = "es"
-        output = self.translate_client.translate(text, target_language=target)
+        output = self.translate_client.translate(self.text, target_language=target)
         return output['translatedText'].lower()
 
-    def hungarian_translator(self, text):
+    def hungarian_translator(self):
         target = "hu"
-        output = self.translate_client.translate(text, target_language=target)
+        output = self.translate_client.translate(self.text, target_language=target)
         return output['translatedText'].lower()
 
 
-translator = Translator()
+translator = Translator(text)
 
 #choosing spanish as an example
 
 if __name__ == '__main__':
-    print(translator.spanish_translator("blue"))
+    print(translator.spanish_translator())
